f2d4d7baf4cf6107db4ccb5adeab8bbb53554f4a
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-07-18 23:24:34 -0300

    :wrench: chore: configured vitest ambient

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,33 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { svelteTesting } from '@testing-library/svelte/vite'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), svelteTesting()],
 	test: {
 		projects: [
 			{
 				extends: './vite.config.ts',
 				test: {
 					name: 'client',
-					environment: 'browser',
+					environment: 'jsdom',
 					browser: {
-						enabled: true,
+						enabled: false,
 						provider: 'playwright',
 						instances: [{ browser: 'chromium' }]
 					},
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
-					exclude: ['src/lib/server/**'],
-					setupFiles: ['./vitest-setup-client.ts']
-				}
-			},
-			{
-				extends: './vite.config.ts',
-				test: {
-					name: 'server',
-					environment: 'node',
 					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts'],
 				}
 			}
-		]
+		],
+		coverage: {
+			provider: 'v8',
+			reporter: ['html'],
+			include: ["src/**/*.{js,ts,svelte}"],
+			exclude: ["**/*.stories.svelte"]
+		}
 	}
 });
